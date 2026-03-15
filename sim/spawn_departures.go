checkandmate1/vice

commit 423dd0b1b2d98cce934045ed935e55c9f09c59c8

sim: don't set departure scratchpad unless explicitly specified
Setting it based on the exit fix nadvertently overrode all of the available facility adaptations to
have it be a single character for the exit fix plus the requested altitude, etc.

sim/spawn_departures.go
@@ -605,7 +605,7 @@ func (s *Sim) createIFRDepartureNoLock(departureAirport string, runway av.Runway
 	nasFp.EntryFix = util.Select(len(ac.FlightPlan.DepartureAirport) == 4, ac.FlightPlan.DepartureAirport[1:],
 		ac.FlightPlan.DepartureAirport)
 	nasFp.ExitFix = shortExit
-	nasFp.Scratchpad = util.Select(dep.Scratchpad != "", dep.Scratchpad, s.State.FacilityAdaptation.Scratchpads[shortExit])
+	nasFp.Scratchpad = dep.Scratchpad
 	nasFp.SecondaryScratchpad = dep.SecondaryScratchpad
 	nasFp.RequestedAltitude = ac.FlightPlan.Altitude
 	nasFp.AssignedAltitude = util.Select(!isTRACON, ac.FlightPlan.Altitude, 0)

commit 3060244d3bdaafe963f94759569d9682cd8a9462

Fix scratchpad assignment bug introduced in 423dd0b1b2d98cce934045ed935e55c9f09c59c8

sim/spawn_departures.go
@@ -635,7 +635,16 @@ func (s *Sim) createIFRDepartureNoLock(departureAirport string, runway av.Runway
 	nasFp.EntryFix = util.Select(len(ac.FlightPlan.DepartureAirport) == 4, ac.FlightPlan.DepartureAirport[1:],
 		ac.FlightPlan.DepartureAirport)
 	nasFp.ExitFix = shortExit
-	nasFp.Scratchpad = dep.Scratchpad
+	if dep.Scratchpad != "" { // this has top priority
+		nasFp.Scratchpad = dep.Scratchpad
+	} else if sp1 := s.State.FacilityAdaptation.Datablocks.Scratchpad1; sp1.DisplayExitFix ||
+		sp1.DisplayExitFix1 || sp1.DisplayExitGate || sp1.DisplayAltExitGate {
+		// Don't set the scratchpad; it will be set automatically.
+	} else if sp, ok := s.State.FacilityAdaptation.Scratchpads[string(dep.Exit)]; ok {
+		nasFp.Scratchpad = sp
+	} else {
+		nasFp.Scratchpad = s.State.FacilityAdaptation.Scratchpads[shortExit]
+	}
 	nasFp.SecondaryScratchpad = dep.SecondaryScratchpad
 	nasFp.RequestedAltitude = ac.FlightPlan.Altitude
 	nasFp.AssignedAltitude = util.Select(!isTRACON, ac.FlightPlan.Altitude, 0)